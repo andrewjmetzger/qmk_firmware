b070e514055088197ebd58d995e480968c7e0a08
Andrew Metzger <590246+andrewjmetzger@users.noreply.github.com>
2025-03-30 16:35:46 -0400

    Enable command keys (QK_MAKE) Remove basd keycode MAGIC_E

--- keyboards/gmmk/pro/rev1/ansi/keymaps/andrewjmetzger/config.h
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/andrewjmetzger/config.h
@@ -3,4 +3,3 @@
 #define LED_MATRIX_DEFAULT_ON true // Sets the default enabled state, if none has been set
 #define LED_MATRIX_DEFAULT_MODE LED_MATRIX_SOLID // Sets the default mode, if none has been set
 #define LED_MATRIX_DEFAULT_VAL LED_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
-
